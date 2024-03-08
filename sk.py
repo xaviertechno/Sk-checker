d18ce5a8c4995d9dc49fff3b5f3d7e5d3a71af2b
Techno Tinker <140100992+xaviertechno@users.noreply.github.com>
2024-03-08 10:19:21 +0530

    Update sk.py

--- sk.py
+++ sk.py
@@ -20,16 +20,17 @@ response = requests.post(url, auth=(sk, ''), data=card_data)
 
 print(response.text)
 
+credits="Code By Techno Tinker"
 
 if 'tok_' in response.text:
-    print("#Aprovadas", sk, "『 ★ SK LIVE ★ 』")
+    print("#Aprovadas", sk, "『 ★ SK LIVE ★ 』 " ,credits)
 elif 'api_key_expired' in response.text:
-    print("#Reprovadas", sk, "『 ★ KEY EXPIRED ★ 』")
+    print("#Reprovadas", sk, "『 ★ KEY EXPIRED ★ 』" ,credits)
 elif 'testmode_charges_only' in response.text:
-    print("#Reprovadas", sk, "『 ★ TEST MODE CHARGES ONLY ★ 』")
+    print("#Reprovadas", sk, "『 ★ TEST MODE CHARGES ONLY ★ 』" ,credits)
 elif 'Invalid API Key' in response.text:
-    print("#Reprovadas", sk, "『 ★ INVALID API KEY ★ 』")
+    print("#Reprovadas", sk, "『 ★ INVALID API KEY ★ 』" ,credits)
 elif 'You must pass full card details to create a token.' in response.text:
-    print("#Reprovadas", sk, "『 ★ CHANGE CC ★ 』" )    
+    print("#Reprovadas", sk, "『 ★ CHANGE CC ★ 』" ,credits)    
 else:
-    print("#Reprovadas", sk, "『 ★ Error Not Listed ★ 』")
+    print("#Reprovadas", sk, "『 ★ Error Not Listed ★ 』" ,credits)
